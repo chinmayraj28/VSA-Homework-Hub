3cfc40eb0cc8c7e00a39eb79786a5f51b9b4cff5
Chinmay Raj Nataraj <70143561+chinmayraj28@users.noreply.github.com>
2024-02-09 22:49:29 +0530

    Update firebase.js

--- handlers/firebase/firebase.js
+++ handlers/firebase/firebase.js
@@ -1,10 +1,10 @@
 // Firebase Realtime Database
 const admin = require('firebase-admin')
-var serviceAccount = require("./vsa-homework-firebase-adminsdk-zlqau-10a7db19a2.json");
+var serviceAccount = require("./.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://vsa-homework-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: "firebasedatabase.app"
 });
 
 let fb = admin.database();
